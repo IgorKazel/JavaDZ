614c62891fd9174535bc30240aa08c718ea31ff1
Igor Kazel <igor.joer@gmail.com>
2023-02-20 20:19:35 +0300

    Lesson 2 DZ bug fixed

--- Lesson2/Task1/Program.java
+++ Lesson2/Task1/Program.java
@@ -1,19 +1,23 @@
 public class Program {
     static void formatString(String str) {
-        String str2 = str.replaceAll(" +", " ").trim();
-        StringBuilder sb = new StringBuilder(str2);
+        String strResult = str.replaceAll(" +", " ").trim();
+        StringBuilder sb = new StringBuilder(strResult);
         for (int i = sb.length() - 1; i >= 1; i--){
-            if (str2.charAt(i) >= 'А' && str2.charAt(i) <= 'Я'){
-                sb.setCharAt(i-1,'.');
-                sb.insert(i, ' ');
+            if (strResult.charAt(i) >= 'А' && strResult.charAt(i) <= 'Я'){
+                if (strResult.charAt(i - 1) == ' ') {
+                    sb.insert(i - 1, '.');
+                } else {
+                    sb.insert(i, ". ");
+                }
             }
         }
         sb.append('.');
-        System.out.println("\nВыходная строка:");
-        System.out.println(sb);
+        strResult = sb.toString();
+        System.out.println("\nПреобразованная строка:");
+        System.out.println(strResult);
     }    
     public static void main(String[] args) {
-        String str = "   Добрый  день     Как   дела   Все     хорошо   ";
+        String str = "   Добрый  деньКак   дела   Все     хорошо   ";
         System.out.println("Входная строка:");
         System.out.println(str);
         formatString(str);
